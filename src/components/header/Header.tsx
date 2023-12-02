17d57587750ded7fdcbe735c8e5871c02a0ae59a
unknown <m60mor@gmail.com>
2023-12-02 12:43:47 +0100

    Initial header design

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,9 @@ import './Header.css';
 export default function Header() {
     return (
         <div className='header'>
+            <div className='header-cable'></div>
             <div className='header-logo'>Kablex</div>
+            <div className='header-cable'></div>
         </div>
     );
 }
