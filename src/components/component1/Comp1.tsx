129b36d1ece98c7058f7b3d0ce73e5a4ed81e7ea
unknown <m60mor@gmail.com>
2023-12-02 12:21:01 +0100

    Updating names of components

--- src/components/component1/Comp1.tsx
+++
@@ -1,7 +0,0 @@
-import './Comp1.css';
-
-export default function Comp1() {
-    return (
-        <div></div>
-    );
-}
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import './Header.css';
+
+export default function Comp1() {
+    return (
+        <div className='header'>
+            <div className='header-logo'></div>
+
+        </div>
+    );
+}
